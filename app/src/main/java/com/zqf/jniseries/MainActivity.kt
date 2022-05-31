56752ffcce6a67330151adda355b29a18564dfc0
zhangqingfa <zhang.qingfa@foundbyte.com>
2022-05-31 16:45:08 +0800

    修改mainActivity

--- app/src/main/java/com/zqf/jniseries/MainActivity.kt
+++ app/src/main/java/com/zqf/jniseries/MainActivity.kt
@@ -8,25 +8,16 @@ import com.zqf.jniseries.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     external fun stringFromJNI(): String
 
     companion object {
-        // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
         }
