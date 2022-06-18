f4b10b235cc360c0b6021333d3130c8557726684
zhangqingfa <1583510917@qq.com>
2022-06-18 12:20:56 +0800

    提交native方法

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,6 +14,6 @@ Java_com_zqf_jniseries_MainActivity_stringFromJNI(
  */
 extern "C" JNIEXPORT void JNICALL
 Java_com_zqf_jniseries_MainActivity_jniVariableStateMent(JNIEnv *env, jobject thiz) {
-
+ // 方法编写
 
 }
