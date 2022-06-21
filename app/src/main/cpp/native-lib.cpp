406fbbaa30e3a3f57610513bc45876d48a0d3df1
zhangqingfa <1583510917@qq.com>
2022-06-21 11:30:48 +0800

    native方法增加

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,6 +14,25 @@ Java_com_zqf_jniseries_MainActivity_stringFromJNI(
  */
 extern "C" JNIEXPORT void JNICALL
 Java_com_zqf_jniseries_MainActivity_jniVariableStateMent(JNIEnv *env, jobject thiz) {
- // 方法编写
+    // 方法编写
 
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_zqf_jniseries_MainActivity_stringFromJNI2(JNIEnv *env, jobject thiz) {
+    // ndk 反射修改java层的变量textStr的数据
+    jclass mainActivityClass = env->GetObjectClass(thiz);
+    jfieldID jtext = env->GetFieldID(mainActivityClass, "textStr", "Ljava/lang/String;");
+    jstring jstr = env->NewStringUTF("Native层修改java层初始化数据了");
+    env->SetObjectField(thiz, jtext, jstr);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_zqf_jniseries_MainActivity_stringFromJNI3(JNIEnv *env, jobject thiz) {
+    // ndk 反射修改java层的变量textStr的数据
+    jclass mainActivityClass = env->GetObjectClass(thiz);
+    jmethodID jmethodId = env->GetMethodID(mainActivityClass, "fun3NativeToJava", "()V");
+    env->CallVoidMethod(thiz, jmethodId);
 }
--- app/src/main/java/com/zqf/jniseries/MainActivity.kt
+++ app/src/main/java/com/zqf/jniseries/MainActivity.kt
@@ -2,30 +2,53 @@ package com.zqf.jniseries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.zqf.jniseries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.sampleText.text = stringFromJNI()
-    }
-
-    external fun stringFromJNI(): String
-
-    /**
-     *  在Native中声明方法
-     */
-    external fun jniVariableStateMent()
-
-    // 静态变量
+    //静态方法 类似java 的 static{}
     companion object {
         init {
             System.loadLibrary("native-lib")
         }
     }
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var textStr = "java层初始化的值"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun fun1click(view: View) {
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    fun fun2click(view: View) {
+        stringFromJNI2()
+        binding.sampleText.text = textStr
+    }
+
+    fun fun3click(view: View) {
+        stringFromJNI3()
+    }
+
+    fun fun3NativeToJava() {
+        Toast.makeText(this, "Native调Java的函数了", Toast.LENGTH_SHORT).show()
+    }
+
+    //java 调 native 函数
+    external fun stringFromJNI(): String
+
+    //native 调java 变量并修改
+    external fun stringFromJNI2()
+
+    //native 调java 函数
+    external fun stringFromJNI3()
 }
