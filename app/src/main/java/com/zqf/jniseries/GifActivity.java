084e699055a65d610cf987c13c74c840ef99f967
zhangqingfa <1583510917@qq.com>
2022-06-27 14:21:42 +0800

    gif图加载构建

---
+++ app/src/main/java/com/zqf/jniseries/GifActivity.java
@@ -0,0 +1,24 @@
+package com.zqf.jniseries;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author: zqf
+ * Date: 2022/06/27
+ * Gif图 ndk 加载页
+ */
+public class GifActivity extends AppCompatActivity {
+
+    static {
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gif_layout);
+    }
+}
