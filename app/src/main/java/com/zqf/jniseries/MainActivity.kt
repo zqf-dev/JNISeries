40f4537beaf4b8f9fc9d9b85d8e6247b1d9fe7bb
zhangqingfa <1583510917@qq.com>
2022-07-14 11:22:45 +0800

    调整传值参数

--- app/src/main/java/com/zqf/jniseries/MainActivity.kt
+++ app/src/main/java/com/zqf/jniseries/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
             intArrayOf(1, 2, 3),
             arrayOf<String>("哈哈", "走你", "测试"),
             booleanArrayOf(true, false),
-            User(20220630, "张三", 20, true)
+            User(20220720, "张三", 28, true)
         )
     }
 
