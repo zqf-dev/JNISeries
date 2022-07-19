385603467b49834af5ca02b0f28c2e91302c104c
zhangqingfa <1583510917@qq.com>
2022-07-19 11:22:22 +0800

    调整gif的native方法

--- app/src/main/java/com/zqf/jniseries/gif/GifHandler.java
+++ app/src/main/java/com/zqf/jniseries/gif/GifHandler.java
@@ -3,7 +3,8 @@ package com.zqf.jniseries.gif;
 import android.graphics.Bitmap;
 
 public class GifHandler {
-    long _gifHandler;//地址 指针类型大小一样的 纽带
+    //地址 指针类型大小一样的 纽带
+    long _gifHandler;
 
     static {
         System.loadLibrary("native-lib");
@@ -31,16 +32,19 @@ public class GifHandler {
         return gifHandler;
     }
 
-    //   开始加载gif文件  Java+包名+类名+方法名  中间分隔用下划线        参数   第一个 JNIEnv  第二个  如static   class   对象  object
-//   第三个开始是需要   传递
+    // 开始加载gif文件  Java+包名+类名+方法名  中间分隔用下划线
+    // 参数
+    // 第一个 JNIEnv
+    // 第二个  如static   class   对象  object
+    // 第三个 开始是需要   传递
     public static native long loadGif(String path);
 
-    //    宽
+    // 宽
     public static native int getWidth(long gifHander);
 
-    //    高
+    // 高
     public static native int getHeight(long gifPoint);
 
-    //    渲染图片
+    // 渲染图片
     public static native int updateFrame(long gifPoint, Bitmap bitmap);
 }
